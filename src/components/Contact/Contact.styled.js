1ad24057a744199a57c460f856d6161e1b46bda2
AntonKh <leaterood@gmail.com>
2022-12-22 22:34:23 +0300

    add styles1

--- src/components/Contact/Contact.styled.js
+++ src/components/Contact/Contact.styled.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 export const Item = styled.li`
+  padding: 10px 20px;
   display: flex;
+  flex-direction: column;
   gap: 10px;
   justify-content: start;
-  align-items: center;
+  align-items: start;
+  background-color: rgba(0, 0, 0, 0.05);
 `;
 export const Info = styled.p``;
 export const Button = styled.button`
@@ -13,6 +16,8 @@ export const Button = styled.button`
   padding: 5px 55px;
   border-radius: 5px;
   border: 1px solid rgba(0, 0, 0, 0.6);
+  background-color: rgb(255, 255, 255);
+
   cursor: pointer;
   &:hover {
     background-color: rgba(135, 206, 250, 0.7);
