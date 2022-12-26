deea10dfa2115791cf07495151e61521d9f9b552
AntonKh <leaterood@gmail.com>
2022-12-26 18:46:04 +0300

    local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,22 @@ import { ContactList } from './ContactList/ContactList';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contactsStorage = JSON.parse(localStorage.getItem('contacts'));
+    if (contactsStorage) {
+      this.setState({ contacts: contactsStorage });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   deleteContact = contactId => {
     console.log(contactId);
