b2c628e4d2f8a6532d162afa0ebb1bb3ab44fb17
AntonKh <leaterood@gmail.com>
2022-12-30 16:57:48 +0300

    finish

--- src/components/AddForm/AddForm.jsx
+++ src/components/AddForm/AddForm.jsx
@@ -1,68 +1,69 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Title, Form, Input, Button, Label } from './AddForm.styled';
 
-export class AddForm extends Component {
-  state = {
-    name: '',
-    number: '',
-    id: '',
+export const AddForm = ({ onFormSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const inputChange = event => {
+    const { name, value } = event.currentTarget;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        break;
+    }
   };
 
-  inputChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  addContactSubmit = event => {
+  const addContactSubmit = event => {
     event.preventDefault();
-
-    this.props.onFormSubmit(this.state);
-    this.reset();
+    onFormSubmit({ name, number });
+    reset();
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
-
-  render() {
-    const { name, number } = this.state;
-
-    return (
-      <>
-        <Title>Phonebook</Title>
-        <Form onSubmit={this.addContactSubmit}>
-          <Label>
-            Name
-            <Input
-              value={name}
-              onChange={this.inputChange}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </Label>
-          <Label>
-            Phone Number
-            <Input
-              value={number}
-              onChange={this.inputChange}
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </Label>
-          <Button type="submit">Add Contact</Button>
-        </Form>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Title>Phonebook</Title>
+      <Form onSubmit={addContactSubmit}>
+        <Label>
+          Name
+          <Input
+            value={name}
+            onChange={inputChange}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </Label>
+        <Label>
+          Phone Number
+          <Input
+            value={number}
+            onChange={inputChange}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </Label>
+        <Button type="submit">Add Contact</Button>
+      </Form>
+    </>
+  );
+};
 
 AddForm.propTypes = {
   onFormSubmit: PropTypes.func,
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,81 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import shortid from 'shortid';
-
 import { Section } from './Section/Section';
 import { AddForm } from './AddForm/AddForm';
 import { SearchForm } from './SearchForm/SearchForm';
 import { ContactList } from './ContactList/ContactList';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) || []
+  );
+  const [filter, setFilter] = useState('');
+  // useEffect(() => {
+  //   JSON.parse(localStorage.getItem('contacts')) &&
+  //     setContacts(JSON.parse(localStorage.getItem('contacts')));
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
   };
 
-  componentDidMount() {
-    const contactsStorage = JSON.parse(localStorage.getItem('contacts'));
-    if (contactsStorage) {
-      this.setState({ contacts: contactsStorage });
-    }
-  }
-
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  deleteContact = contactId => {
-    console.log(contactId);
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const changeFilter = event => {
+    setFilter(event.target.value);
   };
 
-  changeFilter = event => {
-    this.setState({ filter: event.target.value });
-  };
-
-  onFormSubmit = contact => {
+  const onFormSubmit = contact => {
     contact.id = shortid.generate();
 
-    if (this.state.contacts.some(el => el.name === contact.name)) {
+    if (contacts.some(el => el.name === contact.name)) {
       alert(`Contact with name ${contact.name} already exists`);
       return;
     }
 
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts],
-    }));
+    setContacts([contact, ...contacts]);
   };
 
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
+  const getFilteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  render() {
-    const { filter } = this.state;
-    const filteredContacts = this.getFilteredContacts();
-    //есть компонент секции который используется несколько раз, но я хочу для каждой секции задать отдельные стили, как так сделать
-    return (
-      <>
-        <Section>
-          <AddForm onFormSubmit={this.onFormSubmit} />
-        </Section>
-        <Section>
-          <SearchForm filter={filter} changeFilter={this.changeFilter} />
-        </Section>
-        <Section>
-          <ContactList
-            contacts={filteredContacts}
-            onDeleteContact={this.deleteContact}
-          />
-        </Section>
-      </>
-    );
-  }
-}
+  const filteredContacts = getFilteredContacts();
+
+  return (
+    <>
+      <Section>
+        <AddForm onFormSubmit={onFormSubmit} />
+      </Section>
+      <Section>
+        <SearchForm filter={filter} changeFilter={changeFilter} />
+      </Section>
+      <Section>
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={deleteContact}
+        />
+      </Section>
+    </>
+  );
+};
