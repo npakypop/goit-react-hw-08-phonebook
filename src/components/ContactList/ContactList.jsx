91db85cd11361c5a7f82534135c9202116a19de0
AntonKh <leaterood@gmail.com>
2023-01-11 16:59:29 +0300

    try

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,33 +1,33 @@
-import PropTypes from 'prop-types';
-import { Contact } from '../Contact/Contact';
-import { Title, List } from './ContactsList.styled';
+// import PropTypes from 'prop-types';
+// import { Contact } from '../Contact/Contact';
+// import { Title, List } from './ContactsList.styled';
 
-export const ContactList = ({ contacts, onDeleteContact }) => {
-  return (
-    <>
-      <Title>Contacts</Title>
-      <List>
-        {contacts.map(({ id, name, number }) => (
-          <Contact
-            key={id}
-            id={id}
-            name={name}
-            number={number}
-            onDeleteContact={onDeleteContact}
-          />
-        ))}
-      </List>
-    </>
-  );
-};
+// export const ContactList = ({ contacts, onDeleteContact }) => {
+//   return (
+//     <>
+//       <Title>Contacts</Title>
+//       <List>
+//         {contacts.map(({ id, name, number }) => (
+//           <Contact
+//             key={id}
+//             id={id}
+//             name={name}
+//             number={number}
+//             onDeleteContact={onDeleteContact}
+//           />
+//         ))}
+//       </List>
+//     </>
+//   );
+// };
 
-ContactList.propTypes = {
-  onDeleteContact: PropTypes.func.isRequired,
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    }).isRequired
-  ).isRequired,
-};
+// ContactList.propTypes = {
+//   onDeleteContact: PropTypes.func.isRequired,
+//   contacts: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//       number: PropTypes.string.isRequired,
+//     }).isRequired
+//   ).isRequired,
+// };
