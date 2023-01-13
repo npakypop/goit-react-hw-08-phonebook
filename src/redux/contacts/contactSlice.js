b477a73f8c44fbe852c8ac1b4b357f5c88c33494
AntonKh <leaterood@gmail.com>
2023-01-13 13:32:54 +0300

    persist combine

--- src/redux/contacts/contactSlice.js
+++ src/redux/contacts/contactSlice.js
@@ -1,15 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const contactsInitialState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
 const contactsSlice = createSlice({
-  name: 'contacts123123',
-  initialState: { contacts: contactsInitialState },
+  name: 'contacts',
+  initialState: { contacts: [] },
   reducers: {
     addContact(state, { payload }) {
       if (state.contacts.some(el => el.name === payload.name)) {
